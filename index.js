a9c8bff5193047742a02cc0ee92b0a1f7fec10c1
xs-pedia-cyber <applemusicccc5@gmail.com>
2026-08-29 20:21:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.json({
 app.use(cookieParser());
 app.set('trust proxy', 1);
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://arulz-xd-owner:Haqqi0213@cluster0.fgxhxqm.mongodb.net/?appName=Cluster0'; 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://db_ryuuxiao:db_ryuuxiao@cluster0.sj28lxh.mongodb.net/?appName=Cluster0'; 
 
 mongoose.connect(MONGODB_URI)
     .then(() => console.log('📦 Berhasil terhubung ke MongoDB!'))
