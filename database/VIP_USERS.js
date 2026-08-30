22ea648d1606c76712d15c2818d5840c4ee7fddf
xs-pediaa-cyber <applemusicccc5@gmail.com>
2026-08-30 11:32:14 +0700

    Update VIP_USERS.js

--- database/VIP_USERS.js
+++ database/VIP_USERS.js
@@ -1,6 +1,6 @@
 const VIP_USERS = {
     "arulzzzxd": "arulz-vip-123",
-    "haqqi.official13@gmail.com": "vip-special-arulz"
+    "alfatharraby05@gmail.com": "vip-special-arulz"
 };
 
 module.exports = VIP_USERS;
